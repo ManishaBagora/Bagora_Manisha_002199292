52c43c87b90a6aff68b21450567ca76ef68289b9
Manisha Bagora <bagora.m@northastern.edu>
2021-09-27 03:11:19 +0530

    Assignment 1 Java

---
+++ Createprofile/src/model/Product.java
@@ -0,0 +1,182 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Uttam Bagora
+ */
+public class Product {
+    private String name;
+    private String geographicdata;
+    private String dateofbirth;
+    private String telephonenumber;
+    private String faxnumber;
+    private String emailaddresses;
+    private String socialsecuritynumber;
+    private String mediarecordnumber;
+    private String healthplanbeneficiarynumber;
+    private String bankaccountnumber;
+    private String certificatelicensenumber;
+    private String vehicleidentifier;
+    private String deviceidentifier;
+    private String linkedin;
+    private String internetprotocoladdress;
+    private String Biometricidentifier;
+    private String fullfacephoto;
+    private String uniqueidentifier;
+
+    public String getFaxnumber() {
+        return faxnumber;
+    }
+
+    public void setFaxnumber(String faxnumber) {
+        this.faxnumber = faxnumber;
+    }
+
+    public String getEmailaddresses() {
+        return emailaddresses;
+    }
+
+    public void setEmailaddresses(String emailaddresses) {
+        this.emailaddresses = emailaddresses;
+    }
+
+    public String getSocialsecuritynumber() {
+        return socialsecuritynumber;
+    }
+
+    public void setSocialsecuritynumber(String socialsecuritynumber) {
+        this.socialsecuritynumber = socialsecuritynumber;
+    }
+
+    public String getMediarecordnumber() {
+        return mediarecordnumber;
+    }
+
+    public void setMediarecordnumber(String mediarecordnumber) {
+        this.mediarecordnumber = mediarecordnumber;
+    }
+
+    public String getHealthplanbeneficiarynumber() {
+        return healthplanbeneficiarynumber;
+    }
+
+    public void setHealthplanbeneficiarynumber(String healthplanbeneficiarynumber) {
+        this.healthplanbeneficiarynumber = healthplanbeneficiarynumber;
+    }
+
+    public String getBankaccountnumber() {
+        return bankaccountnumber;
+    }
+
+    public void setBankaccountnumber(String bankaccountnumber) {
+        this.bankaccountnumber = bankaccountnumber;
+    }
+
+    public String getCertificatelicensenumber() {
+        return certificatelicensenumber;
+    }
+
+    public void setCertificatelicensenumber(String certificatelicensenumber) {
+        this.certificatelicensenumber = certificatelicensenumber;
+    }
+
+    public String getVehicleidentifier() {
+        return vehicleidentifier;
+    }
+
+    public void setVehicleidentifier(String vehicleidentifier) {
+        this.vehicleidentifier = vehicleidentifier;
+    }
+
+    public String getDeviceidentifier() {
+        return deviceidentifier;
+    }
+
+    public void setDeviceidentifier(String deviceidentifier) {
+        this.deviceidentifier = deviceidentifier;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getInternetprotocoladdress() {
+        return internetprotocoladdress;
+    }
+
+    public void setInternetprotocoladdress(String internetprotocoladdress) {
+        this.internetprotocoladdress = internetprotocoladdress;
+    }
+
+    public String getBiometricidentifier() {
+        return Biometricidentifier;
+    }
+
+    public void setBiometricidentifier(String Biometricidentifier) {
+        this.Biometricidentifier = Biometricidentifier;
+    }
+
+    public String getFullfacephoto() {
+        return fullfacephoto;
+    }
+
+    public void setFullfacephoto(String fullfacephoto) {
+        this.fullfacephoto = fullfacephoto;
+    }
+
+    public String getUniqueidentifier() {
+        return uniqueidentifier;
+    }
+
+    public void setUniqueidentifier(String uniqueidentifier) {
+        this.uniqueidentifier = uniqueidentifier;
+    }
+            
+           
+
+           
+          
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicdata() {
+        return geographicdata;
+    }
+
+    public void setGeographicdata(String geographicdata) {
+        this.geographicdata = geographicdata;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(String dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public String getTelephonenumber() {
+        return telephonenumber;
+    }
+
+    public void setTelephonenumber(String telephonenumber) {
+        this.telephonenumber = telephonenumber;
+    }
+     
+    
+    
+}
